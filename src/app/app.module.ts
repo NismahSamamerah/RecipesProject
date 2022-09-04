4166a63170047f5ff5ef04817097fa75d6d71230
NismahSamamerah <nismahsamamerah@gmail.com>
2022-09-04 11:13:14 +0300

    initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { SuggestComponent } from './components/suggest/suggest.component';
+import { FavoriteComponent } from './components/favorite/favorite.component';
+import { RecipeComponent } from './components/recipe/recipe.component';
+import { CocktailComponent } from './components/cocktail/cocktail.component';
+import { DetailsComponent } from './components/details/details.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { AuthComponent } from './services/auth/auth.component';
+import { ApiComponent } from './services/api/api.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
+    SuggestComponent,
+    FavoriteComponent,
+    RecipeComponent,
+    CocktailComponent,
+    DetailsComponent,
+    ContactUsComponent,
+    AuthComponent,
+    ApiComponent
   ],
   imports: [
     BrowserModule,
