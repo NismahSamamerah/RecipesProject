001693a67aadb9a5aa8e45d2f44502ac21c870dc
NismahSamamerah <nismahsamamerah@gmail.com>
2022-09-09 17:06:56 +0300

    recipes cocktails pages and recipe form with validation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CocktailComponent } from './components/cocktail/cocktail.component';
+import { LoginComponent } from './components/login/login.component';
+import { RecipeFormComponent } from './components/recipe-form/recipe-form.component';
+import { RecipeComponent } from './components/recipe/recipe.component';
+import { RegisterComponent } from './components/register/register.component';
 import { UserCocktailComponent } from './components/user-cocktail/user-cocktail.component';
 
 const routes: Routes = [
   {path : "cocktail" , component : CocktailComponent},
   {path : "user-cocktail" , component : UserCocktailComponent},
+  {path : "recipe-form" , component : RecipeFormComponent},
+  {path : "recipe" , component : RecipeComponent},
+  {path : "register" , component : RegisterComponent},
+  {path : "login" , component : LoginComponent}
 ];
 
 @NgModule({
--- src/app/components/recipe/recipe.component.ts
+++ src/app/components/recipe/recipe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe.component.css']
 })
 export class RecipeComponent implements OnInit {
-  
-  constructor() { }
+
+  constructor(public route :Router) { }
 
   ngOnInit(): void {
+  }
+  goToUserRecipes(){
+    this.route.navigate(['/user-recipe'])
+
   }
 
 }
