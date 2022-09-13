0bf6c211846da25df0803de1cc0105213e9d722a
NooR-Owaina <n.oweinah@gmail.com>
2022-09-14 02:29:10 +0300

    thr same

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,32 +4,32 @@ import { AuthService } from './services/auth.service';
 import { UserService } from './services/user.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'recipesProject';
+    title = 'recipesProject';
 
-  // constructor(public auth:AuthService, private userService: UserService){
-  
+    // constructor(public auth:AuthService, private userService: UserService){
 
-  //   this.auth.user.subscribe(user => {
-  //     this.auth.userID = user?.uid;
-  //     console.log(user?.uid);
-  //   });
-    
- 
 
-  //   this.auth.register(user.email, user.password).then(res => {
-  //     this.userService.saveUserInfo(user).then(res => {
-  //       console.log(res);
-  //     }).catch(err => {
-  //       console.log(err);
-  //     })
-  //   }).catch(err => {
-  //     console.log(err);
-  //   })
+    //   this.auth.user.subscribe(user => {
+    //     this.auth.userID = user?.uid;
+    //     console.log(user?.uid);
+    //   });
+
+
+
+    //   this.auth.register(user.email, user.password).then(res => {
+    //     this.userService.saveUserInfo(user).then(res => {
+    //       console.log(res);
+    //     }).catch(err => {
+    //       console.log(err);
+    //     })
+    //   }).catch(err => {
+    //     console.log(err);
+    //   })
 
 
 
@@ -42,8 +42,8 @@ export class AppComponent {
 
 
 
-  
-  ngOnInit() :void{
 
-  }
+    ngOnInit(): void {
+
+    }
 }
