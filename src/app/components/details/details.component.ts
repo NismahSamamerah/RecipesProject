6c1bdc48f138cf966ad96752ceb7fbb9e7f63e80
NismahSamamerah <nismahsamamerah@gmail.com>
2022-09-14 03:04:00 +0300

    some edits

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,22 +1,37 @@
 import { HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { IComment } from 'src/app/interfaces/comment';
+import { CommentService } from 'src/app/services/comment.service';
+import { AuthService } from 'src/app/services/auth.service';
 import { Route, Router } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
 import { RecipeComponent } from '../recipe/recipe.component';
 
 @Component({
-  selector: 'app-details',
-  templateUrl: './details.component.html',
-  styleUrls: ['./details.component.css']
+    selector: 'app-details',
+    templateUrl: './details.component.html',
+    styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
   recipeTitle?:string;
 recipe :any=[]
   activeRouter: any;
+    commentForm: FormGroup = new FormGroup({
+        comment: new FormControl('', [Validators.required,
+        ]),
+    });
 
-  constructor(public route : Router , public apiService :ApiService , public recipeComponent : RecipeComponent) { }
+  constructor(public route : Router , public apiService :ApiService , public recipeComponent : RecipeComponent
 
-  ngOnInit(): void {
+   , private commentService: CommentService,
+        private auth: AuthService) {
+        const sub = this.auth.user.subscribe(user => {
+            this.auth.userID = user?.uid;
+            sub.unsubscribe();
+        })
+    }
+    ngOnInit(): void {
     // this.recipeTitle = String(this.activeRouter.snapshot.paramMap.get('id'));
     //     if (this.recipeTitle) {
     //         const queryParams = new HttpParams().set('', this.recipeTitle)
@@ -30,6 +45,24 @@ recipe :any=[]
     //         )
     //     }
 
-  }
+    }
+
+    setRate(rate: number) {
+        console.log(rate);
+        //TODO:
+    }
+    saveComment() {
+        const comment: IComment = {
+            type_id: '',//TODO:
+            user_id: this.auth.userID as string,
+            comment: this.commentForm.value.comment,
+            type: 'recipe'
+        }
+        this.commentService.saveCommentInfo(comment).then(res => {
+            console.log(comment);
+        }).catch(err => {
+            console.log(err);
+        })
+    }
 
 }
--- src/app/components/recipe-form/recipe-form.component.ts
+++ src/app/components/recipe-form/recipe-form.component.ts
@@ -1,42 +1,83 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
+import { IRecipe } from 'src/app/interfaces/recipe';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
+
 @Component({
-  selector: 'app-recipe-form',
-  templateUrl: './recipe-form.component.html',
-  styleUrls: ['./recipe-form.component.css']
+    selector: 'app-recipe-form',
+    templateUrl: './recipe-form.component.html',
+    styleUrls: ['./recipe-form.component.css']
 })
 export class RecipeFormComponent implements OnInit {
-  recipeForm: FormGroup = new FormGroup({
-    name: new FormControl('', [
-      Validators.required,
-    ]),
-    ingredients:  new FormArray([]),
-    servings: new FormControl('', [Validators.required
-    ]),
-    instructions:new FormControl('', [Validators.required
-    ]),
-  });
+    recipeForm: FormGroup = new FormGroup({
+        name: new FormControl('', [
+            Validators.required,
+        ]),
+        ingredients: new FormArray([]),
+        servings: new FormControl('', [Validators.required
+        ]),
+        instructions: new FormControl('', [Validators.required
+        ]),
+    });
 
-  ingredientItem: FormGroup = new FormGroup({
-    item: new FormControl(''),
-});
+    ingredientItem: FormGroup = new FormGroup({
+        item: new FormControl(''),
+    });
 
-  constructor() { }
+    constructor(private userService: UserService,
+        private auth: AuthService) {
+        const sub = this.auth.user.subscribe(user => {
+            this.auth.userID = user?.uid;
+            sub.unsubscribe();
+        })
+    }
 
-  ngOnInit(): void {
-  }
-  submitRecipe(newRecipe :any){}
+    saveRecipe() {
+        const recipe: IRecipe = {
+            id: this.generateID(),
+            user_id: this.auth.userID as string,
+            title: this.recipeForm.value.name,
+            ingredients: this.getIngredientsArrayValues(),
+            servings: this.recipeForm.value.servings,
+            instructions: this.recipeForm.value.instructions
+        }
+        console.log(recipe);
+        this.userService.saveRecipeInfo(recipe).then(res => {
+            console.log(recipe);
+        }).catch(err => {
+            console.log(err);
+        })
+    }
 
-  addNewIngredient(){
-    (this.recipeForm.get('ingredients') as FormArray).push(new FormGroup({
-      item: new FormControl(''),
-  }));
-  console.log(this.ingredientItem);
-  }
+    ngOnInit(): void {
+    }
+    submitRecipe(newRecipe: any) { }
 
-  getIngredientsArray() {
-    const array =  this.recipeForm.get('ingredients') as FormArray;
-    return array.controls as FormGroup[];
-}
+    addNewIngredient() {
+        (this.recipeForm.get('ingredients') as FormArray).push(new FormGroup({
+            item: new FormControl(''),
+        }));
+    }
+
+    getIngredientsArray() {
+        const array = this.recipeForm.get('ingredients') as FormArray;
+        return array.controls as FormGroup[];
+    }
+
+    getIngredientsArrayValues() {
+        const arr: string[] = [];
+        for (let item of this.getIngredientsArray()) {
+            arr.push(item.value.item);
+        }
+        return arr;
+    }
+    generateID() {
+        let m = 9; 
+        let s = '', r = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        for (var i=0; i < m; i++) { s += r.charAt(Math.floor(Math.random()*r.length)); }
+        return s;
+    }
+
 }
